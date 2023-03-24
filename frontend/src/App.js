714f9f2067b8c987748e27ed654472c5d5233f67
Vishaka Mohan <vishaka.mohan2019@vitstudent.ac.in>
2023-03-24 15:44:03 +0530

    landing page navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
+import { Landing, Present, Future } from './components';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+    <Router>
+
+      <div className="App">
+        <div className="content">
+
+          <Routes>
+
+            <Route
+              exact
+              path="/"
+              element={
+                <>
+                  <Landing />
+                </>
+              }
+            />
+
+          <Route
+              exact
+              path="/present"
+              element={
+                <>
+                  <Present />
+                </>
+              }
+            />
+
+          <Route
+              exact
+              path="/future"
+              element={
+                <>
+                  <Future />
+                </>
+              }
+            />
+
+
+          </Routes>
+
+
+
+
+        </div>
+
+            
+      </div>
+
+    </Router>
+    
   );
 }
 
